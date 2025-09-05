e1f08ab87b53f46343ebd1908deab95e1b80633e
sriharisrihari50873-cmyk <sriharisrihari50873@gmail.com>
2025-09-05 11:05:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate(operator) {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Please enter valid numbers.";
+  } else {
+    switch (operator) {
+      case '+': result = num1 + num2; break;
+      case '-': result = num1 - num2; break;
+      case '*': result = num1 * num2; break;
+      case '/': 
+        result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+        break;
+    }
+  }
+
+  document.getElementById('result').textContent = "Result: " + result;
+}
